2603833dc222d0dd18bb7f5482e4cb9096aa4334
Lucas Lopes <lp.skt25@gmail.com>
2026-01-10 01:52:21 -0300

    chore: Estrutura inicial

---
+++ js/component-loader.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    const components = document.querySelectorAll('[data-component]');
+
+    components.forEach(async (el) => {
+
+        const name = el.dataset.component;
+
+        try{
+            const html = await fetch(`/components/${name}/${name}.html`);
+            el.innerHTML = await html.text();
+
+            const css = document.createElement("link");
+            css.rel = "stylesheet";
+            css.href = `/components/${name}/${name}.css`;
+            document.head.appendChild(css);
+
+            const js = document.createElement('script');
+            js.src = `/components/${name}/${name}.js`;
+            js.defer = true;
+            document.body.appendChild(js);
+        } catch (err){
+            console.error(`Erro ao carregar o componente: ${name}`, err);
+        }
+    });
+
+});
